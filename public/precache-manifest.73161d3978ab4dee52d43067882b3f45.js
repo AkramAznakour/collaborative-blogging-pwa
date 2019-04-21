7488024d948f3eaa65d0e252467497d04fbe998b
AkramAznakour <akram.aznakour@gmail.com>
2019-04-21 20:43:01 +0100

    changing env variable for heroku 3

---
+++ public/precache-manifest.73161d3978ab4dee52d43067882b3f45.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1d347b10d2464921181",
+    "url": "/css/app.7438c597.css"
+  },
+  {
+    "revision": "a1d347b10d2464921181",
+    "url": "/js/app.959c3098.js"
+  },
+  {
+    "revision": "8433652fef413a4d69ec",
+    "url": "/js/chunk-05411e50.4a2ec694.js"
+  },
+  {
+    "revision": "6d2404a5b7e26af738cf",
+    "url": "/js/chunk-06cb8bc0.59e9d2e4.js"
+  },
+  {
+    "revision": "6f77205ce49aa2346cd1",
+    "url": "/js/chunk-06cbd36b.f1e8587f.js"
+  },
+  {
+    "revision": "f402581ce91fa6e5b8f6",
+    "url": "/js/chunk-14beeb9a.b649dba0.js"
+  },
+  {
+    "revision": "fcf533f5c891d298d09f",
+    "url": "/js/chunk-2ca043d1.c2fd98b3.js"
+  },
+  {
+    "revision": "4e579806816f3b41ab75",
+    "url": "/js/chunk-2d0af0ae.3a86c7ba.js"
+  },
+  {
+    "revision": "3a384894ec61acc2e00f",
+    "url": "/js/chunk-2d0b168d.89ee68fb.js"
+  },
+  {
+    "revision": "3613047b767f803184af",
+    "url": "/js/chunk-2d0c13aa.84022b46.js"
+  },
+  {
+    "revision": "634ef8d561d658f58114",
+    "url": "/js/chunk-2d0ceb36.ebe8da44.js"
+  },
+  {
+    "revision": "78290c17757062be044c",
+    "url": "/js/chunk-2d0d042a.6872e63e.js"
+  },
+  {
+    "revision": "eb50af834fcb9b9a2f15",
+    "url": "/js/chunk-2d0f00b9.461e9371.js"
+  },
+  {
+    "revision": "28fb40ca993d1e3ffed5",
+    "url": "/js/chunk-2d20861f.6f0d1a15.js"
+  },
+  {
+    "revision": "e6f2303126314ea079db",
+    "url": "/js/chunk-74d97018.c66d4bcb.js"
+  },
+  {
+    "revision": "48b718f7a6fe8c76362e",
+    "url": "/css/chunk-vendors.0c974d66.css"
+  },
+  {
+    "revision": "48b718f7a6fe8c76362e",
+    "url": "/js/chunk-vendors.1e4bfda5.js"
+  },
+  {
+    "revision": "8d4a4e6f7431d0d7fa92b1df20f38161",
+    "url": "/fonts/fontello.8d4a4e6f.woff2"
+  },
+  {
+    "revision": "e73a0647198cfe970de0f003be95cc51",
+    "url": "/fonts/fontello.e73a0647.eot"
+  },
+  {
+    "revision": "068ca2b316db98037bebdd1e4f1b9459",
+    "url": "/fonts/fontello.068ca2b3.ttf"
+  },
+  {
+    "revision": "a782baa8633b1359f9686ffad17e0d76",
+    "url": "/fonts/fontello.a782baa8.woff"
+  },
+  {
+    "revision": "9354499c2824248511adf85fdf8e4c37",
+    "url": "/img/fontello.9354499c.svg"
+  },
+  {
+    "revision": "3401ca9bd336348c29b04e7b7c41f620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
